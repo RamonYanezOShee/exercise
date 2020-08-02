93b08905cff92e78c9c0e2727e4959c9d529306e
unknown <ramon.yanez@gmail.com>
2020-08-01 20:16:14 -0400

    Creating base clases for this project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PersonController } from './modules/controllers/person/person.controller';
+import { PersonService } from './modules/services/person/person.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, PersonController],
+  providers: [AppService, PersonService],
 })
 export class AppModule {}
