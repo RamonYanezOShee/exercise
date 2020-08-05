50f872d625ca8f8c33f49c4a1e9fbe46200ec8fe
unknown <ramon.yanez@gmail.com>
2020-08-04 20:15:06 -0400

    fix merge #2

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,15 +9,9 @@ import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-<<<<<<< HEAD
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-	  type: 'mysql',
-=======
-    ConfigModule.forRoot(),    
-    TypeOrmModule.forRoot({
+      ConfigModule.forRoot(),
+      TypeOrmModule.forRoot({
       type: 'mysql',
->>>>>>> feature/8080
       host: process.env.DATABASE_HOST,
       port: parseInt(process.env.DATABASE_PORT),
       username: process.env.DATABASE_USER,
